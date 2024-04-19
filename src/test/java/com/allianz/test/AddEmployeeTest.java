8b28f4b927f64e4f6c9699ff7ebac47d6c01f9ab
Sujatha Nair <sujatha.pnair@gmail.com>
2024-04-18 21:10:09 -0700

    file upload task

--- src/test/java/com/allianz/test/AddEmployeeTest.java
+++ src/test/java/com/allianz/test/AddEmployeeTest.java
@@ -1,6 +1,9 @@
 package com.allianz.test;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.allianz.base.AutomationWrapper;
@@ -10,9 +13,17 @@ public class AddEmployeeTest extends AutomationWrapper {
 	
 	public void uploadInvalidDisplayPicture()
 	{
+		File file = new File ("src/test/resources/files/Sample text.txt");
+		String path = file.getAbsolutePath();
 		driver.findElement(By.name("username")).sendKeys("Admin");;
 		driver.findElement(By.name("password")).sendKeys("admin123");;
 		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
+		driver.findElement(By.xpath("//span[text()='PIM']")).click();
+		driver.findElement(By.linkText("Add Employee")).click();
+		driver.findElement(By.xpath("//input[@type='file' and @class ='oxd-file-input']")).sendKeys(path);
+		String actualError = driver.findElement(By.xpath("//span[contains(normalize-space(),'File type')]")).getText();
+		Assert.assertTrue(actualError.contains("File type not allowed"));
+		
 		
 	}
 
